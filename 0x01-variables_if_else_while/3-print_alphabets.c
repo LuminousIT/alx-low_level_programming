830707ae260330de492597766e448b1179f4998e
Olumide Oluwole <oluwoleit@gmail.com>
2023-03-21 02:06:50 +0100

    all caps

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: 'print all alphabeths in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char start = 'a';
+
+	while (start <= 'z')
+	{
+		putchar(start);
+		start++;
+	}
+
+	putchar('\n');
+
+	start = "A";
+
+	while (start <= 'Z')
+	{
+		putchar(start);
+		start++;
+	}
+
+	return (0);
+}
