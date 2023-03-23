fa97693f91366a4282fb211e74b2c81ff466cad9
Olumide Oluwole <oluwoleit@gmail.com>
2023-03-23 03:56:23 +0100

    sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry Point
+ *
+ * Description: return sum of two ints
+ * @a: int a
+ * @b: int b
+ * Return: void
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
