9d3ce944a9f4d3faafafa0a17c20b0fd51ec76c3
Olumide Oluwole <oluwoleit@gmail.com>
2023-03-23 02:27:59 +0100

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry Point
+ *
+ * Description: return last digit of any int
+ * @n: accepts n as an int
+ * Return: int | last digit
+ */
+int print_last_digit(int n)
+{
+	int answer = n % 10;
+
+	return (answer);
+}
