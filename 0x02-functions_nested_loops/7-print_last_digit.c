fe70fa031f8c8e7b6f945f179b12b5d07c455ceb
Olumide Oluwole <oluwoleit@gmail.com>
2023-03-23 02:44:07 +0100

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,6 +2,7 @@
 #include "main.h"
 
 int _abs(int);
+
 /**
  * print_last_digit - Entry Point
  *
@@ -13,9 +14,17 @@ int print_last_digit(int n)
 {
 	int answer = _abs(n % 10);
 
+	_putchar('0' + answer);
 	return (answer);
 }
 
+/**
+ * _abs - Entry Point
+ *
+ * Description: return asb val of any digit
+ * @n: accepts n as an int
+ * Return: int | abs digit
+ */
 int _abs(int n)
 {
 	return (abs(n));
