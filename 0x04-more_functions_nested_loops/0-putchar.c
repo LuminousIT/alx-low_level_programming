2ecea2a4ce2d16d3e0626ffb1d268e85e349c644
Olumide Oluwole <oluwoleit@gmail.com>
2023-03-24 21:45:17 +0100

    print more numbers

--- 0x04-more_functions_nested_loops/0-putchar.c
+++ 0x04-more_functions_nested_loops/0-putchar.c
@@ -11,6 +11,7 @@
 
 int main(void)
 {
-	print_most_numbers();
+	// print_most_numbers();
+	more_numbers();
 	return (0);
 }
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry Point
+ *
+ * Description: 'function that prints 10 times the numbers, from 0 to 14,
+ * followed by a new line '
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int row = 0;
+
+	while (row < 10)
+	{
+		int start = 0;
+
+		while (start <= 14)
+		{
+			if (start < 10)
+			{
+				_putchar('0' + start);
+			}
+			else
+			{
+				_putchar('0' + start / 10);
+				_putchar('0' + start % 10);
+			}
+			start++;
+		}
+		row++;
+		_putchar('\n');
+	}
+}
