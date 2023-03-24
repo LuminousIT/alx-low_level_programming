fdc712bd8ebb49d9e26bda6369e782c6b52d9860
Olumide Oluwole <oluwoleit@gmail.com>
2023-03-24 22:10:03 +0100

    straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,17 +9,12 @@
  */
 void print_line(int n)
 {
-	if (n == 0)
-	{
-		_putchar('\n');
-		return;
-	}
-
 	int start = 0;
 
 	while (start < n)
 	{
-		_putchar('_');
+		if (start > 0)
+			_putchar('_');
 		start++;
 	}
 	_putchar('\n');
