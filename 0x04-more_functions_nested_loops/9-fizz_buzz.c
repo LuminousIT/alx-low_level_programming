675812ec523aafca5f01664e11114bad8273b24b
Olumide Oluwole <oluwoleit@gmail.com>
2023-03-25 00:34:20 +0100

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Description: function to print fizzbuzz
+ * Return: void
+ */
+int main(void)
+{
+	int start = 1;
+
+	while (start <= 100)
+	{
+		if ((start % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((start % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%d ", start);
+		start++;
+	}
+	printf("\n");
+
+	return (0);
+}
