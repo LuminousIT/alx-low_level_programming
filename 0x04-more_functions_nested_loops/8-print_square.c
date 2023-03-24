be3cb7351e3106341797b21feb8806ea4df453d4
Olumide Oluwole <oluwoleit@gmail.com>
2023-03-24 23:54:36 +0100

    print square with #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - Entry Point
+ *
+ * Description: function that draws a square with # in the terminal
+ * @size: accepts int size
+ * Return: void
+ */
+void print_square(int size)
+{
+	int row = 0;
+
+	while (row < size)
+	{
+		int col = 0;
+
+		while (col < size)
+		{
+			_putchar("#");
+			col++;
+		}
+		_putchar('\n');
+		row++;
+	}
+}
