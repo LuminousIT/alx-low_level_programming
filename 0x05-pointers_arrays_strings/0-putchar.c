babe1a4075970605f0923869295d6abdf932d978
Olumide Oluwole <oluwoleit@gmail.com>
2023-03-28 23:18:24 +0200

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Description: 'print _putchar'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	// print_most_numbers();
+	
+	return (0);
+}
