e4fbcc1127d6b864a2e8a828bcbe9c1f2f8ce811
Olumide Oluwole <oluwoleit@gmail.com>
2023-03-29 03:51:14 +0200

    copy vals

--- 0x05-pointers_arrays_strings/0-putchar.c
+++ 0x05-pointers_arrays_strings/0-putchar.c
@@ -12,20 +12,12 @@
 int main(void)
 {
 	// print_most_numbers();
-	char *str;
-	int array[5];
-	array[0] = 98;
-	array[1] = 402;
-	array[2] = -198;
-	array[3] = 298;
-	array[4] = -1024;
+	char s1[98];
+	char *ptr;
 
-	str = "01234567891";
-	print_rev(str);
-	puts2(str);
-	puts_half(str);
-
-	print_array(array, 5);
+	ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+	printf("%s", s1);
+	printf("%s", ptr);
 
 	return (0);
 }
