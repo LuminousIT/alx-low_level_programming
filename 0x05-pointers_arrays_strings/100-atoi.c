1b4c429118e5486b45d50e61dce2203de628669a
Olumide Oluwole <oluwoleit@gmail.com>
2023-03-29 04:10:39 +0200

    keygen

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * _atoi - Entry Point
+ *
+ * Description: 'conv str to int'
+ * @s: accepts *s char
+ * Return: Always 0 (Success)
+ */
 int _atoi(char *s)
 {
 	return atoi(s);
