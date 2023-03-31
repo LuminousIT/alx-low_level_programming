f9b418f227570873bd9c8f2cfee6ab6e121ac5e4
Olumide Oluwole <oluwoleit@gmail.com>
2023-03-31 03:29:25 +0200

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,21 +14,23 @@ char *leet(char *n)
 {
 	int i, a;
 
-	int d;
-	char b[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char c[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char b[11] = "aAeEoOtTlL";
+	char c[11] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
+		d = 1;
 		for (a = 0; a <= 9; a++)
 		{
-			if (b[a] == n[i])
+			if (b[a] == n[i] && d == 1)
 			{
 				n[i] = c[a];
+				d = 0;
 			}
 		}
 	}
 
 	return (n);
 }
+
 
