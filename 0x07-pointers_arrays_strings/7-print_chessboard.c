73ddccd7d334fc54f49eb7ddcfa2fa0a0b0057e6
Olumide Oluwole <oluwoleit@gmail.com>
2023-04-05 02:36:49 +0200

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,13 +10,13 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j = 0;
 
-	int col = sizeof(a[0]);
+	/* int col = sizeof(a[0]); */
 
 	int total = sizeof(a); /* row * col */
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < col; j++)
+		for (j = 0; j < 8; j++)
 		{
 			/* printf("%c", a[i][j]); */
 			_putchar(a[i][j]);
