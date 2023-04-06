48deb5371afce2ed5140281e8d28aff5f5c5c39f
Olumide Oluwole <oluwoleit@gmail.com>
2023-04-07 01:52:56 +0200

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - Entry Point
+ *
+ * Description:  function that prints a string, followed by a new line.
+ * @s: string
+ *
+ * Return: Always void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
