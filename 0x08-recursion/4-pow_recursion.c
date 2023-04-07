c9f9f360a3fff23503afb6c6f78d9e5c4ff0f024
Olumide Oluwole <oluwoleit@gmail.com>
2023-04-07 02:19:18 +0200

    factorial

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "math.h"
+
+/**
+ * _pow_recursion - Entry Point
+ *
+ * Description: function that returns the value of x raised to the power of y.
+ * @x: base
+ * @y: power
+ *
+ * Return: Always int(Success)
+ */
+int _pow_recursion(int x, int y)
+{
+	return (pow(x, y));
+}
