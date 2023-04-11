64d2d3309aad06d8f8d2ed3af7ab3210860d69f4
Olumide Oluwole <oluwoleit@gmail.com>
2023-04-11 23:00:12 +0200

    args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: 'print program name'
+ * @argc: count of args
+ * @argv: array of all args
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
