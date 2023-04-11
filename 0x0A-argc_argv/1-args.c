001f4813d8c9dc37f7f618d7c245173fa281637e
Olumide Oluwole <oluwoleit@gmail.com>
2023-04-11 23:08:46 +0200

    print args count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: 'print arguement count'
+ * @argc: count of args
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
