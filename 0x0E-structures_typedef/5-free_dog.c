8d04d834e21f59277cd2a556ca919d35e8abc058
Olumide Oluwole <oluwoleit@gmail.com>
2023-04-20 01:54:46 +0200

    freeeee

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * free_dog - Define a new function that frees struct
+ * @d: struct
+ *
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
