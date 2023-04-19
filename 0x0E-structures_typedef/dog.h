2b44a340bdb70d55f52f549067aa83a88a5f5490
Olumide Oluwole <oluwoleit@gmail.com>
2023-04-20 01:04:54 +0200

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MYHEADER
+#define MYHEADER
+
+/**
+ * struct dog - Define a new type struct dog
+ * @name: char member
+ * @age: float member
+ * @owner: char member
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
